25e2ce5839d7754bbed6c2ca613e685de5ffdaae
TwinkeToes <115041264+TwinkeToes@users.noreply.github.com>
2024-04-29 15:51:11 -0700

    Add files via upload
    just a commit

--- Middleris.js
+++ Middleris.js
@@ -1,1 +1,6 @@
-alert("djsajd");
+document.addEventListener("DOMContentLoaded", function(){
+    let score = 0;
+    let blockPosition = 0;
+    let shape = Math.floor(Math.random() * 7);
+    console.log(shape);
+});
