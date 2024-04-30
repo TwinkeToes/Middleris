c0f2c0688ddf725cecd2ff2ee31e6fb119c493f8
TwinkeToes <115041264+TwinkeToes@users.noreply.github.com>
2024-04-30 12:30:57 -0700

    Add files via upload
    I created the blocks for the peices, and tried making a moving program

--- Middleris.js
+++ Middleris.js
@@ -1,6 +1,8 @@
 document.addEventListener("DOMContentLoaded", function(){
-    let score = 0;
-    let blockPosition = 0;
-    let shape = Math.floor(Math.random() * 7);
+    const Block = document.querySelector('#Block') // little blocks lol
+    let score = 0; // score for the game
+    let blockPosition = 0; // px position for the blocks
+    let shape = Math.floor(Math.random() * 7); // a variable that picks what shape 
     console.log(shape);
+
 });
