caa3d72a4b2fcf571b22b6811a1ee9b56e930ec5
TwinkeToes <115041264+TwinkeToes@users.noreply.github.com>
2024-05-07 11:00:55 -0700

    Add files via upload
    I made the gravity movement

--- Middleris.js
+++ Middleris.js
@@ -1,8 +1,21 @@
-document.addEventListener("DOMContentLoaded", function(){
-    const Block = document.querySelector('#Block') // little blocks lol
+document.addEventListener("DOMContentLoaded", function () {
+    const Block = document.querySelector('#Block'); // little blocks lol
     let score = 0; // score for the game
     let blockPosition = 0; // px position for the blocks
     let shape = Math.floor(Math.random() * 7); // a variable that picks what shape 
-    console.log(shape);
+    var Interval = setInterval(spostaDiv, 90);
+    var gravity = 0;
 
+    let l = window.screen.width;
+    let w = 130;
+
+    function spostaDiv() {
+        console.log(w);
+        if (gravity >= l) { 
+            gravity = 0;
+        } else {
+            Block.style.marginLeft = gravity + "px";
+        }
+        gravity += 7;
+    } // gravity
 });
